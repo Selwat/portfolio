c07c759452d85eee6ddfd24c1a4590a26e815372
Mikoaj Selwat <selwatmikolaj@gmail.com>
2025-02-05 19:15:57 +0100

    feat: styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-emerald-950">{children}</body>
     </html>
   );
 }
