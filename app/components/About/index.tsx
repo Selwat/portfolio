c2e874150efb94d8e5de282504fabed14a707715
Mikoaj Selwat <selwatmikolaj@gmail.com>
2025-02-10 23:15:46 +0100

    feat: add button to scroll down

--- app/components/About/index.tsx
+++ app/components/About/index.tsx
@@ -5,13 +5,34 @@ import TechLogos from "./skillTiles"
 import RotatingCube from "../../components/RotatingCube"
 
 const About = () => {
-  const scrollToSection = () => {
+  const scrollUp = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     })
   }
-
+  
+  const scrollDown = () => {
+    const sections = document.querySelectorAll(".scroll-section");
+    const currentScroll = window.scrollY;
+    let nextSection = null;
+  
+    for (let section of sections) {
+      if (section.offsetTop > currentScroll + 10) { 
+        nextSection = section;
+        break;
+      }
+    }
+  
+    if (nextSection) {
+      window.scrollTo({
+        top: nextSection.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+  
+  
   return (
     <div className="w-full h-full p-4 md:p-10 flex flex-col items-center xl:justify-between">
       <motion.div
@@ -21,7 +42,7 @@ const About = () => {
         transition={{ duration: 2.5 }}
       >
         <motion.button
-          onClick={scrollToSection}
+          onClick={scrollUp}
           className="bg-white text-gray-900 font-semibold px-4 md:px-6 py-2 rounded-full shadow-lg border-2 border-gray-300 hover:bg-gray-200 transition-all duration-300 flex items-center gap-2"
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
@@ -35,13 +56,33 @@ const About = () => {
           </motion.span>
         </motion.button>
       </motion.div>
-      <div className="w-full h-auto md:h-full px-4 xl:px-10 flex flex-col xl:flex-row gap-6 items-center justify-between">
+      <div className="w-full h-1/3 md:h-full xl:px-10 flex flex-col xl:flex-row gap-6 items-center justify-between">
         <Description />
-        <div className="w-full xl:w-1/3 min-h-[400px] h-1/3">
+        <div className="w-full h-[600px] xl:w-1/3 xl:h-1/2 min-h-[500px] ">
           <RotatingCube />
         </div>
         <TechLogos />
       </div>
+      <motion.div
+        className="hidden xl:flex cursor-pointer"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 2.5 }}
+      >
+        <motion.button
+          onClick={scrollDown}
+          className="bg-white text-gray-900 font-semibold px-4 md:px-6 py-2 rounded-full shadow-lg border-2 border-gray-300 hover:bg-gray-200 transition-all duration-300 flex items-center gap-2"
+
+        >
+          <span>Scroll Down</span>
+          <motion.span
+            animate={{ y: [0, -5, 0] }}
+            transition={{ repeat: Infinity, duration: 1 }}
+          >
+            ⬇️
+          </motion.span>
+        </motion.button>
+      </motion.div>
     </div>
   )
 }
